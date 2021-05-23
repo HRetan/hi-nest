b27cbcaa036cdb3e98d6a5a935dc45ee5cd7bc1a
HRetan <kairencer@gmail.com>
2021-05-24 07:03:50 +0900

    RestApi #2.1

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+
+    @Get()
+    getAll(){
+        return "This will return all movies";
+    }
+
+    @Get("search")
+    search(@Query("year") searchingYear:string, @Query("name") searchingName:string){
+        return `We are searching for a movie made after : ${searchingYear}, name : ${searchingName}`
+    }
+
+    @Get("/:id")
+    getOne(@Param("id") movieId: string){
+        return `This will return one movie with the id : ${movieId}`;    }
+
+    @Post()
+    create(@Body() movieData) {
+        return movieData
+    }
+
+    @Delete("/:id")
+    remove(@Param('id') movieId:string) {
+        return `This will delete a movie with the id: ${movieId}`;  
+    }
+
+    @Patch('/:id')
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updatedMovie: movieId,
+            ...updateData
+        };
+    }
+}
